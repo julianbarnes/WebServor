f1c4134bed6ea33352cdd85aeffb0eca9e7b19d9
julianbarnes <julian.barnes100@gmail.com>
2017-10-29 13:58:04 +0900

    Remoevd fill start and fill end. Also removed the handle method

--- BookWebServer.py
+++ BookWebServer.py
@@ -2,42 +2,32 @@
 from socket import *
 import sys # In order to terminate the program
 import io
+import argparse
 
-def process_type(extension):
-    if extension == "html":
-        pass
-    if extension == "css":
-        pass
-    if extension == "js":
-        pass
-    if extension == "jpg":
-        pass
-    if extension == "png":
-        pass
+	
+
 
 server_socket = socket(AF_INET, SOCK_STREAM)
 server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 #Prepare a sever socket
-#Fill in start
 server_socket.bind(('',12000))
 server_socket.listen(10)
-#Fill in end
 while True:
     #Establish the connection
     print('Ready to serve...')
     connection_socket, addr = server_socket.accept()
-    print("Accepted")  #Fill in start   #Fill in end          
+    print("Accepted")        
     try:
         message = connection_socket.recv(1024).decode()
-        print("Connected")  #Fill in start          #Fill in end               
+        print("Connected")                
         filename = message.split()[1]
         print(filename)
        # filetype = filename.split('.')[1]
        # process_type(filetype, connection_socket)
         #Redirect to root directory                 
         f = io.open(filename[1:], encoding="latin1")
-        print(filename)                        
-        outputdata = f.readlines()#Fill in start       #Fill in end                   
+        print(filename[1:])                        
+        outputdata = f.readlines()                 
         #Send one HTTP header line into socket
         #print(outputdata[0])
         response = """\
@@ -52,18 +42,17 @@ HTTP/1.1 200 OK
         #connection_socket.close()
     except IOError:
         #Send response message for file not found
-        #Fill in start
+        
         print("Exception")
         notfound = """\
             HTTP/1.1 404 Not Found\r\n\r\n
             """
         connection_socket.send(notfound.encode()) 
 
-        #Fill in end
+
         #Close client socket
-        #Fill in start
         connection_socket.close()
-        #Fill in end            
+         
 serverSocket.close()
 print("Closed")
 sys.exit()#Terminate the program after sending the corresponding data                                    
